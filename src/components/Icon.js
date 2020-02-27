2f7db40fe49074df3df8b91c5c09b9fae1d27afd
Christian <chnikel@gmail.com>
2020-02-27 15:15:04 +0100

    Icon pfad übergabe und ansicht optimiert

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,11 +2,21 @@ import React from 'react';
 import iconStyle from './Icon.module.css';
 
 function Icon(props) {
-	const styleOverwrite = {};
+	console.log(props.size);
+
+	const imageStyle = {
+		width: props.size ? props.size.w : '',
+		height: props.size ? props.size.h : '10px'
+	};
 
 	return (
-		<div className={iconStyle.Icon} style={styleOverwrite}>
-			<img className={iconStyle.Icon} src={props.src ? props.src : ''} alt='' />
+		<div className={iconStyle.Icon}>
+			<img
+				className={iconStyle.Icon}
+				src={props.src ? props.src : ''}
+				style={imageStyle}
+				alt=''
+			/>
 		</div>
 	);
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import Icon from './Icon';
-
 import logoStyle from './Logo.module.css';
-
 import logo from '../images/Logo 1@2x.png';
 
 function Logo() {
 	return (
 		<div className={logoStyle.Logo}>
-			<Icon src={logo} />
+			<Icon src={logo} size={{ h: '30px', w: '30px' }} />
 		</div>
 	);
 }
