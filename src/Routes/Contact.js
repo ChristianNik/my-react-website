15d3705af9fda2648bf0cfa98745f46bd30a785e
Christian <chnikel@gmail.com>
2020-03-03 15:25:12 +0100

    Kontakt Komponente erstellt

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+// Styles
+import contactStyle from './Contact.module.css';
+// Icons
+import kontaktIcon from '../images/icons/bxs-envelope.svg';
+import githubIcon from '../images/icons/bxl-github-light.svg';
+import linkedinIcon from '../images/icons/bxl-linkedin.svg';
+// Components
+import Button from '../components/Button';
+import Input from '../components/Input';
+import Icon from '../components/Icon';
 function Contact() {
 	return (
-		<div className='page'>
-			<h1>Contact</h1>
+		<div className={`${contactStyle.Contact} page`}>
+			<h1 className={contactStyle.Contact__title}>Contact</h1>
+			<form action=''>
+				<Input title='Name' />
+				<Input title='E-Mail' />
+				<Input title='Nachricht' />
+
+				<Button title='SENDEN' />
+			</form>
+			<div>
+				<Icon src={kontaktIcon} />
+				<div>kontakt@chnikel.de</div>
+				<Icon src={githubIcon} />
+				<div>ChristianNik</div>
+				<Icon src={linkedinIcon} />
+				<div>christian-nikel-3537a619b</div>
+			</div>
 		</div>
 	);
 }
