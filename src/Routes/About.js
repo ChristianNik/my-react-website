87ecff31e6c72c0e7900587562972086cbf8547f
Christian <chnikel@gmail.com>
2020-03-07 18:23:11 +0100

    Feature: About Page ist teilweise responsive

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -11,11 +11,13 @@ function About() {
 	return (
 		<div className={`${aboutSyle.About} page`}>
 			<h1 className={aboutSyle.About__title}>Über mich</h1>
+
 			<p className={aboutSyle.About__subtitle}>
 				Ich bin Christian. In meiner Freizeit programmiere und designe ich
 				Webseiten. Ich wohne in der Nähe von Köln und bin gerne Unterwegs.
 			</p>
 			<div className={aboutSyle.About__Photo}></div>
+
 			<List className={aboutSyle.About__Hobbys} title='Hobbys'>
 				<IconWithText text='Joggen' />
 				<IconWithText text='Programmieren' />
