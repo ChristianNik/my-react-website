758d54ad2cd97102ae39fb094feaae4bc2024895
Christian <chnikel@gmail.com>
2020-03-10 09:32:06 +0100

    Change: Sidebar verwendet neue art von icons >Icons vievbox angepasst

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,6 +9,7 @@ import LinkItem from './LinkItem';
 import sidebarStyle from './Sidebar.module.css';
 // Icons
 import logoImg from '../images/Logo 1@2x.png';
+import Icon from './icons';
 import {
 	HomeIcon,
 	AboutIcon,
@@ -23,31 +24,31 @@ import {
 function Sidebar() {
 	const START_ICON_SIZE = 36;
 	const CENTER_ICON_SIZE = 28;
-	const END_ICON_SIZE = 18;
+	const END_ICON_SIZE = 24;
 
 	return (
 		<nav className={sidebarStyle.Sidebar}>
 			<LinkGroup>
 				<LinkItem to='/' text='logo' noActiveStyle>
-					<LogoIcon width={START_ICON_SIZE} />
+					<Icon name='logo' width={START_ICON_SIZE} />
 				</LinkItem>
 			</LinkGroup>
 			<LinkGroup position='center'>
 				<LinkItem to='/' text='home'>
-					<HomeIcon width={CENTER_ICON_SIZE} fill='#707070' />
+					<Icon name='home' width={CENTER_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 				<LinkItem to='/about' text='about'>
-					<AboutIcon width={CENTER_ICON_SIZE} fill='#707070' />
+					<Icon name='user' width={CENTER_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 				<LinkItem to='/skills' text='skills'>
-					<SkillsIcon width={CENTER_ICON_SIZE} fill='#707070' />
+					<Icon name='cog' width={CENTER_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 				<LinkItem to='/projects' text='projects'>
-					<ProjectsIcon width={CENTER_ICON_SIZE} fill='#707070' />
+					<Icon name='briefcase' width={CENTER_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 				{/*
 				<LinkItem to='/contact' text='contact'>
-					<ContactIcon width={CENTER_ICON_SIZE} fill='#707070' />
+					<Icon name='envelope' width={CENTER_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 			*/}
 			</LinkGroup>
@@ -56,10 +57,10 @@ function Sidebar() {
 					href='https://www.linkedin.com/in/christian-nikel-3537a619b'
 					text='linkedin'
 				>
-					<LinkedInIcon width={END_ICON_SIZE} fill='#707070' />
+					<Icon name='linkedin' width={END_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 				<LinkItem href='https://github.com/ChristianNik' text='github'>
-					<GithubIcon width={END_ICON_SIZE} fill='#707070' />
+					<Icon name='github' width={END_ICON_SIZE} fill='#707070' />
 				</LinkItem>
 			</LinkGroup>
 		</nav>
