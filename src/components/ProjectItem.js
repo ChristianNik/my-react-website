22c7bde5238b0fc7edb82cdbe1c650ba73ccf6a8
Christian <chnikel@gmail.com>
2020-03-15 15:11:24 +0100

    🔄 Change: ProjectItem überarbeitet

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -8,50 +8,18 @@ import style from './ProjectItem.module.css';
 import Button from '../components/Button';
 
 function ProjectItem(props) {
-	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<div
-			onClick={() => {
-				setIsOpen(!isOpen);
-			}}
-			className={`${style.ProjectItem} ${
-				isOpen ? style.ProjectItem__active : ''
-			}`}
-		>
-			<h3 className={style.ProjectItem__title}>{props.title}</h3>
-			<img className={style.ProjectItem__image} src={props.image} alt='' />
-			<p
-				className={`${style.ProjectItem__discription} ${
-					isOpen ? style.ProjectItem__active : ''
-				}`}
-			>
-				{props.discription}
-			</p>
-			<div
-				className={`${style.ProjectItem__controls} ${
-					isOpen ? style.ProjectItem__active : ''
-				}`}
-			>
-				{props.hrefDemo ? (
-					<Button href={props.hrefDemo} type='primary' text='DEMO' />
-				) : (
-					''
-				)}
-				{props.hrefCode ? (
-					<Button href={props.hrefCode} type='secondary' text='CODE' />
-				) : (
-					''
-				)}
-			</div>
+		<div className={style.ProjectItem}>
+			<img
+				onClick={props.func || ''}
+				className={style.ProjectItem__image || ''}
+				src={props.src || ''}
+				alt=''
+			/>
 		</div>
 	);
 }
 
 export default ProjectItem;
 
-ProjectItem.propTypes = {
-	title: PropTypes.string.isRequired,
-	discription: PropTypes.string,
-	hrefCode: PropTypes.string,
-	hrefDemo: PropTypes.string
-};
+ProjectItem.propTypes = {};
